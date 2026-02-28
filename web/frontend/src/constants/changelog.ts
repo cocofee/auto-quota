1d81a67fbbc3457885c0368be629657ebed802a2
cocofee <41024847@qq.com>
2026-03-01 01:05:53 +0800

    deploy: v0.1.41

--- web/frontend/src/constants/changelog.ts
+++ web/frontend/src/constants/changelog.ts
@@ -5,7 +5,7 @@
  * APP_VERSION 保持和 lzc-manifest.yml 中的 version 一致。
  */
 
-export const APP_VERSION = '0.1.42';
+export const APP_VERSION = '0.1.44';
 
 export interface ChangelogEntry {
   version: string;
@@ -14,6 +14,20 @@ export interface ChangelogEntry {
 }
 
 export const CHANGELOG: ChangelogEntry[] = [
+  {
+    version: '0.1.44',
+    date: '2026-03-01',
+    changes: [
+      'v0.1.44 更新',
+    ],
+  },
+  {
+    version: '0.1.43',
+    date: '2026-03-01',
+    changes: [
+      'v0.1.43 更新',
+    ],
+  },
   {
     version: '0.1.42',
     date: '2026-03-01',
